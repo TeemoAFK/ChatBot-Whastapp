22eddf50732c24e5a1631f921934dd75cf07a057
the97prime <mateo_penaherrera@yahoo.com>
2023-05-10 13:06:13 -0500

    Actualizacion

--- app.js
+++ app.js
@@ -4,63 +4,49 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+const flowSecundario = addKeyword(['menu', 'menu']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
 
-const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
-    [
-        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
-        'https://bot-whatsapp.netlify.app/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
-    [
-        '🙌 Aquí encontras un ejemplo rapido',
-        'https://bot-whatsapp.netlify.app/docs/example/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
-    [
-        '🚀 Puedes aportar tu granito de arena a este proyecto',
-        '[*opencollective*] https://opencollective.com/bot-whatsapp',
-        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
-        '[*patreon*] https://www.patreon.com/leifermendez',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowDiscord = addKeyword(['discord']).addAnswer(
+const flowVentas = addKeyword(['info', 'informacion', 'información']).addAnswer(
     ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
     null,
     null,
     [flowSecundario]
 )
 
+const flowWeb = addKeyword(['2']).addAnswer(
+    ['Visita ', 'https://comercialmichelena.com', '\n*Menu* Para volver al mená principal.'],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowCatalogo = addKeyword(['newsletter', 'news']).addAnswer(
+    'Indica cual es tu email', null, (ctx, { fallBack }) => {
+        if (!ctx.body.includes('@')) return fallBack()}
+)
+
+const flowInmobiliario = addKeyword(['newsletter', 'news']).addAnswer(
+    'Indica cual es tu email', null, (ctx, { fallBack }) => {
+        if (!ctx.body.includes('@')) return fallBack()}
+)
+
 const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
+    .addAnswer('🙌 Hola te has comunicado con Comercial Michelena 👷🏽, tu mejor opción en materiales de construcción 🔨🪛')
     .addAnswer(
         [
-            'Te comparto los siguientes links de interes sobre el proyecto',
-            '👉 *doc* para ver la documentación',
-            '👉 *gracias*  para ver la lista de videos',
-            '👉 *discord* unirte al discord',
+            '¿Como te encuentras?',
+            'Escribe la opcion que te interesa',
+            '👉 *1. *Para comunicarte con ventas 🧱',
+            '👉 *2. *Para solictar nuestro catálogo de productos 🏗',
+            '👉 *3. *Para ver nuestros proyectos Inmobiliarios 🏠 ',
+            '👉 *4. *Para visitar nuestro sitio web 🧑🏽‍💻',
         ],
         null,
         null,
-        [flowDocs, flowGracias, flowTuto, flowDiscord]
+        [flowVentas, flowCatalogo, flowInmobiliario, flowWeb]
     )
+
+
 
 const main = async () => {
     const adapterDB = new MockAdapter()
